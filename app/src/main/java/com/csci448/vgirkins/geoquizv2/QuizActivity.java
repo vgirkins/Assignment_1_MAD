2b6775920b74c57557bfd3d4ac5b72cfa3bdc8de
vgirkins <28710650+vgirkins@users.noreply.github.com>
2018-02-02 13:48:27 -0700

    Added a java file that may or may not be necessary

--- app/src/main/java/com/csci448/vgirkins/geoquizv2/QuizActivity.java
+++ app/src/main/java/com/csci448/vgirkins/geoquizv2/QuizActivity.java
@@ -75,6 +75,35 @@ public class QuizActivity extends AppCompatActivity {
             mIsCheater = CheatActivity.wasAnswerShown(data);
         }
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
